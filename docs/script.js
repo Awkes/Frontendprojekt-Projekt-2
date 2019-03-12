fe1827576e11ec39491a3bc9e2ae76df1445498e
Andreas Åkerlöf <andreas@akerlof.se>
2019-03-12 14:57:25 +0100

    FFunktion för att räkna ut medelökning i procent klar

--- docs/script.js
+++ docs/script.js
@@ -1,3 +1,32 @@
+// Hämta nödvändiga element
+
+// Arrayer för respektive område innehållande kvadratmeterpris för senaste 5 åren i ordningen [2014,2015,2016,2017,2018]
+const Place11122 = [74790,89261,94811,98494,93084];
+const Place22222 = [24361,27320,29922,35750,35920];
+const Place33333 = [3760,4465,4203,5247,6617];
+
+// Funktion som räknar ut procentuell ökning per år och returnerar ett medelvärde på årlig ökning i procent (decimalform)
+function getPercent(prices) {
+    let percents = [];
+    for (let i=0; i < prices.length-1; i++) {
+        let percent = prices[i+1] - prices[i]; // nästa års pris - gammalt pris = ökning
+        percent /= prices[i];                  // ökning / gammalt pris = procentuell ökning
+        percents.push(percent);
+    }
+    return percents.reduce((total,num) => total+num) / percents.length;
+}
+
+// Funktion som räknar ut årlig ökning för kommande 5 år baserat på medelvärdet för ökning i procent
+function calcIncrease(price,area,percent) {
+    return price/area;
+}
+
+console.log(calcIncrease(1000000,40,getPercent(Place11122)));
+
+
+
+
+/*
 // Temporärt script för att visa tabell och toggla table/chart
 const table = document.querySelector('#result-table');
 const chart = document.querySelector('#result-chart');
@@ -22,3 +51,4 @@ resultSwitch.addEventListener('click',e => {
     table.classList.toggle('result-hide');
     chart.classList.toggle('result-hide');
 });
+*/
